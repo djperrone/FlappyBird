40bc827fb78ba70299a62d38194ce959b6179776
djperrone <60362379+djperrone@users.noreply.github.com>
2021-05-22 13:49:42 -0400

    initial setup

---
+++ Flappy/src/com/thecherno/flappy/graphics/Shader.java
@@ -0,0 +1,42 @@
+package com.thecherno.flappy.graphics;
+
+import com.thecherno.flappy.math.Vector3f;
+import com.thecherno.flappy.util.ShaderUtils;
+import static org.lwjgl.opengl.GL20.*;
+
+public class Shader {
+	
+	private int ID;
+	
+	public static Shader BASIC;
+	
+	private Shader(String vertex, String fragment)
+	{
+		ID = ShaderUtils.load(vertex, fragment);
+	}
+	
+	public static void loadAll()
+	{
+		BASIC = new Shader("shaders/shader.vert", "shaders/shader.frag");
+	}
+	
+	public int getUniform(String name)
+	{
+		return glGetUniformLocation(ID,name);
+	}
+	
+	public void setUnfirom3f(String name, Vector3f vector)
+	{
+		glUniform3f(getUniform(name),vector.x,vector.y,vector.z);
+	}
+	
+	public void enable()
+	{
+		glUseProgram(ID);
+	}
+
+	public void disable()
+	{
+		glUseProgram(0);
+	}
+}
